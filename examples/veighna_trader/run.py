d82895a2ed677cc8f136f46c34c5b9504420e541
nimawang <785427452@qq.com>
2026-05-27 17:14:28 +0800

    依赖调整

--- examples/veighna_trader/run.py
+++ examples/veighna_trader/run.py
@@ -28,7 +28,7 @@ from vnpy_ctabacktester import CtaBacktesterApp
 # from vnpy_chartwizard import ChartWizardApp
 # from vnpy_rpcservice import RpcServiceApp
 # from vnpy_excelrtd import ExcelRtdApp
-from vnpy_datamanager import DataManagerApp
+# from vnpy_datamanager import DataManagerApp
 # from vnpy_datarecorder import DataRecorderApp
 # from vnpy_riskmanager import RiskManagerApp
 # from vnpy_webtrader import WebTraderApp
@@ -70,7 +70,7 @@ def main():
     # main_engine.add_app(ChartWizardApp)
     # main_engine.add_app(RpcServiceApp)
     # main_engine.add_app(ExcelRtdApp)
-    main_engine.add_app(DataManagerApp)
+    # main_engine.add_app(DataManagerApp)
     # main_engine.add_app(DataRecorderApp)
     # main_engine.add_app(RiskManagerApp)
     # main_engine.add_app(WebTraderApp)
--- vnpy/alpha/logger.py
+++ vnpy/alpha/logger.py
@@ -1,12 +1,32 @@
 import sys
+from datetime import datetime
+from pathlib import Path
 
 from loguru import logger
+
+from vnpy.trader.setting import SETTINGS
+from vnpy.trader.utility import get_folder_path
 
 
 # Remove default output
 logger.remove()
 
 
-# Add terminal output
+# Log format
 fmt: str = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> <level>{message}</level>"
-logger.add(sys.stdout, colorize=True, format=fmt)
+
+# Log level
+level: int = SETTINGS["log.level"]
+
+# Add console output
+if SETTINGS["log.console"]:
+    logger.add(sys.stdout, colorize=True, format=fmt, level=level)
+
+# Add file output
+if SETTINGS["log.file"]:
+    today_date: str = datetime.now().strftime("%Y%m%d")
+    filename: str = f"alpha_{today_date}.log"
+    log_path: Path = get_folder_path("log")
+    file_path: Path = log_path.joinpath(filename)
+
+    logger.add(sink=file_path, format=fmt, level=level)
